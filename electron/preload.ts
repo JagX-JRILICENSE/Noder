d3814ac8401a131a24ede1095899674390534166
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-07 23:34:26 +0100

    Expose PTY APIs and extensions list via contextBridge

--- electron/preload.ts
+++ electron/preload.ts
@@ -18,8 +18,27 @@ contextBridge.exposeInMainWorld('electronAPI', {
   // App
   getVersion: () => ipcRenderer.invoke('app:getVersion'),
 
+  // Extensions
+  listExtensions: () => ipcRenderer.invoke('extensions:list'),
+
   // Menu events
   onMenuOpenFolder: (callback: () => void) => {
     ipcRenderer.on('menu-open-folder', callback)
+  },
+  onMenuNewTerminal: (callback: () => void) => {
+    ipcRenderer.on('menu-new-terminal', callback)
+  },
+
+  // PTY (full terminal)
+  ptySpawn: (id: string, cwd?: string) => ipcRenderer.invoke('pty:spawn', id, cwd),
+  ptyWrite: (id: string, data: string) => ipcRenderer.send('pty:write', id, data),
+  ptyResize: (id: string, cols: number, rows: number) =>
+    ipcRenderer.send('pty:resize', id, cols, rows),
+  ptyKill: (id: string) => ipcRenderer.invoke('pty:kill', id),
+  onPtyData: (callback: (payload: { id: string; data: string }) => void) => {
+    ipcRenderer.on('pty:data', (_e, payload) => callback(payload))
+  },
+  onPtyExit: (callback: (payload: { id: string }) => void) => {
+    ipcRenderer.on('pty:exit', (_e, payload) => callback(payload))
   },
 })
