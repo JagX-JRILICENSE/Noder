45619d10f693e1eec62d610e0cc2a1d895dc414e
JagX-JRILICENSE <jrilicense@gmail.com>
2026-09-07 23:44:49 +0100

    Expose git, extensions, updater APIs

--- electron/preload.ts
+++ electron/preload.ts
@@ -3,25 +3,24 @@ import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('electronAPI', {
   platform: process.platform,
 
-  // Dialogs
   openFolder: () => ipcRenderer.invoke('dialog:openFolder'),
-
-  // File system
   readDir: (dirPath: string) => ipcRenderer.invoke('fs:readDir', dirPath),
   readFile: (filePath: string) => ipcRenderer.invoke('fs:readFile', filePath),
   writeFile: (filePath: string, content: string) =>
     ipcRenderer.invoke('fs:writeFile', filePath, content),
-
-  // Shell
   openExternal: (url: string) => ipcRenderer.invoke('shell:openExternal', url),
-
-  // App
   getVersion: () => ipcRenderer.invoke('app:getVersion'),
 
   // Extensions
   listExtensions: () => ipcRenderer.invoke('extensions:list'),
+  executeCommand: (commandId: string, ...args: any[]) =>
+    ipcRenderer.invoke('extensions:executeCommand', commandId, ...args),
+  getStatusBarItems: () => ipcRenderer.invoke('extensions:getStatusBarItems'),
+  onExtensionMessage: (cb: (payload: { extensionId: string; message: string }) => void) => {
+    ipcRenderer.on('extension:message', (_e, payload) => cb(payload))
+  },
 
-  // Menu events
+  // Menu
   onMenuOpenFolder: (callback: () => void) => {
     ipcRenderer.on('menu-open-folder', callback)
   },
@@ -29,7 +28,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
     ipcRenderer.on('menu-new-terminal', callback)
   },
 
-  // PTY (full terminal)
+  // PTY
   ptySpawn: (id: string, cwd?: string) => ipcRenderer.invoke('pty:spawn', id, cwd),
   ptyWrite: (id: string, data: string) => ipcRenderer.send('pty:write', id, data),
   ptyResize: (id: string, cols: number, rows: number) =>
@@ -40,5 +39,16 @@ contextBridge.exposeInMainWorld('electronAPI', {
   },
   onPtyExit: (callback: (payload: { id: string }) => void) => {
     ipcRenderer.on('pty:exit', (_e, payload) => callback(payload))
+  },
+
+  // Git
+  gitStatus: (cwd?: string) => ipcRenderer.invoke('git:status', cwd),
+  gitBlame: (filePath: string) => ipcRenderer.invoke('git:blame', filePath),
+
+  // Updater
+  checkForUpdates: () => ipcRenderer.invoke('updater:check'),
+  installUpdate: () => ipcRenderer.invoke('updater:install'),
+  onUpdaterStatus: (cb: (payload: any) => void) => {
+    ipcRenderer.on('updater:status', (_e, payload) => cb(payload))
   },
 })
